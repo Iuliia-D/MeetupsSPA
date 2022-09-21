9adb01a439f46094f9ed8788fd77d4e19378bd90
Iuliia-D <iuliia.derevleva@gmail.com>
2022-09-21 22:13:52 +0200

    add Favorites-context

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
 
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
   return (
-    <div>
-      <Link to="/favorites">Favorites Page</Link>
-    </div>
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
   );
 }
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm.js";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetupsspa-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
