6c06cd8a588172460739b72427a92bf87623f93f
Iuliia-D <iuliia.derevleva@gmail.com>
2022-09-21 14:44:23 +0200

    Add form

---
+++ src/pages/Favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function FavoritesPage() {
+  return (
+    <div>
+      <Link to="/favorites">Favorites Page</Link>
+    </div>
+  );
+}
+
+export default FavoritesPage;
